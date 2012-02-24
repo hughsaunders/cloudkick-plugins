7f719fa678726fa03691869f73ab5deef5152d74
Hugh Saunders <hugh.saunders@rackspace.co.uk>
2012-02-24 20:02:13 +0000

    generalised

--- backup_age.sh
+++ backup_age.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 day_in_seconds=86400
-warn_days=3
-error_days=5
-age_file=/rsnapshot/daily.0/localhost/home/hugh3869/.backup_age
+age_file="$1"
+warn_days="$2"
+error_days="$3"
+
 
 seconds_behind=$(( $(date '+%s') - $(cat $age_file) ))
 
 days_behind=$(echo "scale=4; $seconds_behind/$day_in_seconds"|bc)
+grep '^\.' <<<$days_behind && days_behind="0$days_behind"
 
 status="ok"
-[[ $days_behind > 2 ]] && status="warn"
-[[ $days_behind > 4 ]] && status="err"
+[[ "$days_behind" > 2 ]] && status="warn"
+[[ "$days_behind" > 4 ]] && status="err"
 
 message="backup age file $age_file on $(hostname) is $seconds_behind seconds ($days_behind days) behind the current time"
 
 echo "status $status $message"
 echo "metric secondsbehind int $seconds_behind"
-
 
